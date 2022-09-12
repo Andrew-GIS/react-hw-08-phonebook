f0ea7971e6b009c8611a26dfc24c01ff3f1c6a59
AndrewFedorchenko <fedorchenko.andrey29071995@gmail.com>
2022-09-12 17:55:09 +0300

    basename added

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -5,7 +5,7 @@ import css from './UserMenu.module.css';
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const userName = useSelector(state => state.auth.user.name);
-
+  console.log('userName :>> ', userName);
   return (
     <div className={css.userWrap}>
       <p className={css.authTitle}>
--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import './index.css';
 import { Provider } from "react-redux";
-import { store } from './redux/store';
+import { store, persistor  } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const BaseName = '/react-hw-08-phonebook/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename={BaseName}>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
